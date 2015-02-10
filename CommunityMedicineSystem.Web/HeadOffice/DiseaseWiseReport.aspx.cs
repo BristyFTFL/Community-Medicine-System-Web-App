c449724ac357ae23eec96bf807137e8a70a34a46
BristyFTFL <bristy.cse@gmail.com>
2015-02-10 15:32:35 +0600

    Web Application for CommunityMedicineSystem

---
+++ CommunityMedicineSystem.Web/HeadOffice/DiseaseWiseReport.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CommunityMedicineSystem.BLL;
+using CommunityMedicineSystem.DAO;
+
+namespace CommunityMedicineSystem.Web.HeadOffice
+{
+    public partial class DiseaseWiseReport : System.Web.UI.Page
+    {
+        DiseaseManager aDiseaseManager = new DiseaseManager();
+
+        public void showButton_Click(object sender, EventArgs e)
+        {
+            int diseaseId= Convert.ToInt32(diseaseDropDownList.SelectedValue);
+            showGridView.DataSource = aDiseaseManager.GetTotalPatientList(startDateTextBox.Value, endDateTextBox.Value, diseaseId);
+            showGridView.DataBind();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            diseaseDropDownList.DataSource = aDiseaseManager.GetAllDiseases();
+            diseaseDropDownList.DataTextField = "Name";
+            diseaseDropDownList.DataValueField ="Id";
+            diseaseDropDownList.DataBind();
+        }
+    }
+}
